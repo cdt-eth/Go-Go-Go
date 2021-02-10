26ad9a91ed1274b30c670f3847164bc37e2d22af
christianturner <christiandavidphoto@gmail.com>
2021-02-10 13:39:27 -0500

    first goroutine

--- concurrency/goroutine 2/goroutine.go
+++ concurrency/goroutine 2/goroutine.go
@@ -7,9 +7,14 @@ import (
 
 
 func main(){
-	// this will run forever so `count(fish)` will never run
-	count("sheep")
+	// // this will run forever so `count(fish)` will never run
+	// count("sheep")
+
+	// created a goroutine so that now they can run side-by-side
+	go count("sheep")
 	count("fish")
+
+	// if we ran both functions as goroutines we'd enter a deadlock because both functions are queued and waiting for another line of code that won't run
 }
 
 func count(thing string){
