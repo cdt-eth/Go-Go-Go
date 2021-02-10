0a876c0531e6f150642aba58de12ba47024c96b3
christianturner <christiandavidphoto@gmail.com>
2021-02-09 22:03:10 -0500

    for

--- for/for.go
+++ for/for.go
@@ -5,5 +5,19 @@ import(
 )
 
 func main(){
+	// print 0-5
+	for i := 0; i < 6; i++ {
+		fmt.Println(i)
+	}
 	
+	// newline
+	fmt.Println("\n")
+
+	// create while loop with for loop-like syntax by removing params
+	// prints 1-4
+	j := 1
+	for j < 5 {
+		fmt.Println(j)
+		j++
+	}
 }
